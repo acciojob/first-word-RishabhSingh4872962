31643646167c21db8f9f883eed03a0b7220f5b12
RishabhSingh4872962 <109566428+RishabhSingh4872962@users.noreply.github.com>
2024-02-25 20:10:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstWord(s) {
   // your code here
+	if (s.length==0 || s.length==1) {
+		return s;
+	}
+	let str=s.trim().split(" ");
+	if (str.length==1) {
+		return s;
+	}
+	return str[0];
 }
 
 // Do not change the code below
